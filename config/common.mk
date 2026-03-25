adfd13d3645d708bff74c3ce702c5114ae499a0d
CuriousNom <aryanbiswas7585@gmail.com>
2026-03-25 14:32:56 +0530

    vendor: Convert to aospa vendor
    Change-Id: I2075bb1bfad8153249d33b629dbff49deb98802a

--- config/common.mk
+++ config/common.mk
@@ -4,7 +4,10 @@ $(call inherit-product-if-exists, vendor/extra/product.mk)
 # Allow vendor prebuilt repos to exclude themselves from bp scanning
 -include $(sort $(wildcard vendor/*/*/exclude-bp.mk))
 
-PRODUCT_BRAND ?= LineageOS
+# Inherit common aospa stuff
+$(call inherit-product, vendor/aospa/config/common_aospa.mk)
+
+PRODUCT_BRAND ?= AOSPA
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
@@ -35,7 +38,7 @@ endif
 
 # Lineage-specific broadcast actions whitelist
 PRODUCT_COPY_FILES += \
-    vendor/lineage/config/permissions/lineage-sysconfig.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/lineage-sysconfig.xml
+    vendor/aospa/config/permissions/lineage-sysconfig.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/lineage-sysconfig.xml
 
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
@@ -55,7 +58,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 
 ifneq ($(TARGET_DISABLE_LINEAGE_SDK), true)
 # Lineage SDK
-include vendor/lineage/config/lineage_sdk_common.mk
+include vendor/aospa/config/lineage_sdk_common.mk
 endif
 
 # Do not include art debug targets
@@ -109,10 +112,10 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 endif
 
 # System overlays
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/lineage/overlay/no-rro
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/aospa/overlay/no-rro
 PRODUCT_PACKAGE_OVERLAYS += \
-    vendor/lineage/overlay/common \
-    vendor/lineage/overlay/no-rro
+    vendor/aospa/overlay/common \
+    vendor/aospa/overlay/no-rro
 
 PRODUCT_PACKAGES += \
     DocumentsUIOverlay \
@@ -130,8 +133,8 @@ CUSTOM_LOCALES += \
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/crowdin/overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/crowdin/overlay
 
-include vendor/lineage/config/version.mk
+include vendor/aospa/config/version.mk
 
--include vendor/lineage-priv/keys/keys.mk
+-include vendor/pa-priv/keys/keys.mk
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
